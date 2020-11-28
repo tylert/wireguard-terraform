7910dd6c19e4a17d3a3de91ca0a0e6aade93d2e6
Tyler Tidman <tyler.tidman@draak.ca>
2020-11-27 19:29:02 -0500

    Start adding in the VPC flow log magic too

---
+++ vpc/flow_logs.tf
@@ -0,0 +1,82 @@
+/*
+  __ _                    _
+ / _| | _____      __    | | ___   __ _ ___
+| |_| |/ _ \ \ /\ / /____| |/ _ \ / _` / __|
+|  _| | (_) \ V  V /_____| | (_) | (_| \__ \
+|_| |_|\___/ \_/\_/      |_|\___/ \__, |___/
+                                  |___/
+*/
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/flow_log
+
+resource "aws_iam_role" "main" {
+  name = "main"  # name_prefix
+
+  tags = {
+    Name = "${var.basename}-role"
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "main" {
+  name = "main"  # name_prefix
+  role = aws_iam_role.main.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "main" {
+  name              = "main"  # name_prefix
+  retention_in_days = 7       # default???
+
+  tags = {
+    Name = "${var.basename}-loggroup"
+  }
+}
+
+resource "aws_flow_log" "main" {
+  vpc_id                   = aws_vpc.main.id
+  traffic_type             = "ALL"
+  log_destination          = aws_cloudwatch_log_group.main.arn
+  iam_role_arn             = aws_iam_role.main.arn
+  max_aggregation_interval = 600
+
+  tags = {
+    Name = "${var.basename}-flowlog"
+  }
+}
