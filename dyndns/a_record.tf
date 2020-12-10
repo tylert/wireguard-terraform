d673d1cfc91cbedcb1c3a72dd15e2e4df71716d5
Tyler Tidman <tyler.tidman@draak.ca>
2020-12-10 16:36:13 -0500

    Try to make the dyndns thing work even if it has been run before

--- dyndns/a_record.tf
+++ dyndns/a_record.tf
@@ -1,5 +1,9 @@
 # https://medium.com/@matzhouse/dynamic-dns-with-terraform-and-route53-3fafe7c68970
 
+# https://registry.terraform.io/providers/hashicorp/http/latest/docs/data-sources/http
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/route53_zone
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
+
 data "http" "myip" {
   url = var.dyndns_url
 }
@@ -10,9 +14,10 @@ data "aws_route53_zone" "selected" {
 }
 
 resource "aws_route53_record" "home" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "${var.record_name}.${var.hosted_zone_name}"
-  type    = "A"
-  ttl     = var.ttl
-  records = [chomp(data.http.myip.body)]
+  zone_id         = data.aws_route53_zone.selected.zone_id
+  name            = "${var.record_name}.${var.hosted_zone_name}"
+  type            = "A"
+  ttl             = var.ttl
+  records         = [chomp(data.http.myip.body)]
+  allow_overwrite = true
 }
