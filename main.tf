e4dd377371bee94292323f6a3f3a890ac2d166cf
Tyler Tidman <tyler.tidman@draak.ca>
2020-12-21 15:18:19 -0500

    Move to terraform 0.14.3

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@
 */
 
 terraform {
-  required_version = "~> 0.14.2"
+  required_version = "~> 0.14.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.21.0"
+      version = "~> 3.22.0"
     }
     http = {
       source  = "hashicorp/http"
