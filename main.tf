5edd596f019a2409204fafb7690205c141063f36
Tyler Tidman <tyler.tidman@draak.ca>
2021-01-25 19:49:59 -0500

    Move to latest TF stuff

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@
 */
 
 terraform {
-  required_version = "~> 0.14.4"
+  required_version = "~> 0.14.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.23.0"
+      version = "~> 3.25.0"
     }
     http = {
       source  = "hashicorp/http"
