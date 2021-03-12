2d50da94e55ea83119ecb1f10758a5984c450f8f
Tyler Tidman <tyler.tidman@draak.ca>
2021-03-11 19:42:20 -0500

    Update TF and providers

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@
 */
 
 terraform {
-  required_version = "~> 0.14.7"
+  required_version = "~> 0.14.8"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.30.0"
+      version = "~> 3.31.0"
     }
     http = {
       source  = "hashicorp/http"
