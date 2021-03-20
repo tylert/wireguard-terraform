af65a7981b8f983a65af9cfe8e646af1b9110205
Tyler Tidman <tyler.tidman@draak.ca>
2021-03-20 14:00:52 -0400

    Bump TF provider versions

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.31.0"
+      version = "~> 3.33.0"
     }
     http = {
       source  = "hashicorp/http"
