8e7c6efd9e5ca2444f9404d15192c02915d54629
Tyler Tidman <tyler.tidman@draak.ca>
2021-06-25 16:29:39 -0400

    Bump to latest tf and providers

--- external_ip/providers.tf
+++ external_ip/providers.tf
@@ -8,12 +8,12 @@
 */
 
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.0.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.46.0"
+      version = "~> 3.47.0"
     }
     http = {
       source  = "hashicorp/http"
