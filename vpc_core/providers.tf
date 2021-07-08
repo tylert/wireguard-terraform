497bd09c9d583aef215726d6507d23bcafcc696b
Tyler Tidman <tyler.tidman@draak.ca>
2021-07-08 08:03:39 -0400

    Bump provider verions to latest

--- vpc_core/providers.tf
+++ vpc_core/providers.tf
@@ -8,12 +8,12 @@
 */
 
 terraform {
-  required_version = "~> 1.0.1"
+  required_version = "~> 1.0.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.47.0"
+      version = "~> 3.48.0"
     }
   }
 }
