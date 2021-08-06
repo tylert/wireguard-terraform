6db5f27b3f2f32a59428bebacbaa947ad3404890
Tyler Tidman <tyler.tidman@draak.ca>
2021-08-06 16:29:10 -0400

    Move to latest TF and providersg

--- vpc_core/providers.tf
+++ vpc_core/providers.tf
@@ -8,12 +8,12 @@
 */
 
 terraform {
-  required_version = "~> 1.0.3"
+  required_version = "~> 1.0.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.52.0"
+      version = "~> 3.53.0"
     }
   }
 }
