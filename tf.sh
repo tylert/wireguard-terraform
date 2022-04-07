b9c1a467aabff503768f889f4c3d06eb45894e1a
Tyler Tidman <tyler.tidman@draak.ca>
2022-04-07 10:20:07 -0400

    Fix up the tf wrapper a bit and make fmt less silly

--- tf.sh
+++ tf.sh
@@ -16,214 +16,229 @@
 # IFS=$'\n\t'
 
 
-# Call 'terraform init' for a module
+# Call 'terraform init' for a directory
 tf_init() {
-    local module="${1}"
-    local conf_file="${2}"
+    local directory="${1}"
+    local var_file="${2}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
     fi
 
-    if [ -z "${conf_file}" ]; then
-        conf_file='../backend.tfvars'
+    if [ -z "${var_file}" ]; then
+        var_file='terraform.tfvars'
     fi
 
-    if [ -e "${conf_file}" ]; then
-        "${TERRAFORM}"                     \
-            -chdir="${module}"             \
-            init                           \
-            -backend-config="${conf_file}" \
-            -input=false                   \
+    if [ -e "${var_file}" ]; then
+        "${TERRAFORM}"                    \
+            -chdir="${directory}"         \
+            init                          \
+            -backend-config="${var_file}" \
+            -input=false                  \
             -upgrade=true
     else
-        "${TERRAFORM}"         \
-            -chdir="${module}" \
-            init               \
-            -input=false       \
+        "${TERRAFORM}"            \
+            -chdir="${directory}" \
+            init                  \
+            -input=false          \
             -upgrade=true
     fi
 }
 
 
-# Call 'tflint --init' for a module
+# Call 'tflint --init' for a directory
 tf_lint_init() {
-    local module="${1}"
+    local directory="${1}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
     fi
 
-    "${TFLINT}" --init "${module}"
+    "${TFLINT}" --init "${directory}"
 }
 
 
-# Call 'tflint' for a module
+# Call 'tflint' for a directory
 tf_lint() {
-    local module="${1}"
+    local directory="${1}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
     fi
 
-    "${TFLINT}" "${module}"
+    "${TFLINT}" "${directory}"
 }
 
 
-# Call 'terraform validate' for a module
+# Call 'terraform validate' for a directory
 tf_validate() {
-    local module="${1}"
+    local directory="${1}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
     fi
 
-    "${TERRAFORM}"         \
-        -chdir="${module}" \
+    "${TERRAFORM}"            \
+        -chdir="${directory}" \
         validate
 }
 
 
-# Call 'terraform version' for a module
+# Call 'terraform version' for a directory
 tf_version() {
-    local module="${1}"
+    local directory="${1}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
     fi
 
-    "${TERRAFORM}"         \
-        -chdir="${module}" \
+    "${TERRAFORM}"            \
+        -chdir="${directory}" \
         version
 }
 
 
-# Call 'terraform fmt' for all files in a module
+# Call 'terraform fmt' for all files in a directory
 tf_fmt() {
-    local module="${1}"
+    local directory="${1}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
     fi
 
     # Only .tf and .tfvars files can be processed with terraform fmt
-    for file in "${module}"/*.tf; do
+    for file in $(find "${directory}" -name '*.tf' -o -name '*.tfvars'); do
         "${TERRAFORM}" fmt "${file}"
     done
 }
 
 
-# Call 'terraform plan' for a module
+# Call 'terraform plan' for a directory
 #     XXX FIXME TODO  Combine tf_plan and tf_plan_destroy functions???
 tf_plan() {
-    local module="${1}"
-    local plan_file="${2}"
-    local var_file="${3}"
+    local directory="${1}"
+    local var_file="${2}"
+    local plan_file="${3}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
-    fi
-
-    if [ -z "${plan_file}" ]; then
-        plan_file='../plan_file'
     fi
 
     if [ -z "${var_file}" ]; then
         var_file='terraform.tfvars'
     fi
 
+    if [ -z "${plan_file}" ]; then
+        plan_file='plan_file'
+    fi
+
     if [ -e "${var_file}" ]; then
         "${TERRAFORM}"              \
-            -chdir="${module}"      \
+            -chdir="${directory}"   \
             plan                    \
             -input=false            \
             -out="${plan_file}"     \
             -var-file="${var_file}"
     else
-        "${TERRAFORM}"          \
-            -chdir="${module}"  \
-            plan                \
-            -input=false        \
+        "${TERRAFORM}"            \
+            -chdir="${directory}" \
+            plan                  \
+            -input=false          \
             -out="${plan_file}"
     fi
 }
 
 
-# Call 'terraform plan -destroy' for a module
+# Call 'terraform plan -destroy' for a directory
 #     XXX FIXME TODO  Combine tf_plan and tf_plan_destroy functions???
 tf_plan_destroy() {
-    local module="${1}"
-    local plan_file="${2}"
-    local var_file="${3}"
+    local directory="${1}"
+    local var_file="${2}"
+    local plan_file="${3}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
-    fi
-
-    if [ -z "${plan_file}" ]; then
-        plan_file='../plan_file'
     fi
 
     if [ -z "${var_file}" ]; then
         var_file='terraform.tfvars'
     fi
 
+    if [ -z "${plan_file}" ]; then
+        plan_file='plan_file'
+    fi
+
     if [ -e "${var_file}" ]; then
         "${TERRAFORM}"              \
-            -chdir="${module}"      \
+            -chdir="${directory}"   \
             plan                    \
             -destroy                \
             -input=false            \
             -out="${plan_file}"     \
             -var-file="${var_file}"
     else
-        "${TERRAFORM}"          \
-            -chdir="${module}"  \
-            plan                \
-            -destroy            \
-            -input=false        \
+        "${TERRAFORM}"            \
+            -chdir="${directory}" \
+            plan                  \
+            -destroy              \
+            -input=false          \
             -out="${plan_file}"
     fi
 }
 
 
-# Call 'terraform apply' for a module
+# Call 'terraform apply' for a directory
 tf_apply() {
-    local module="${1}"
+    local directory="${1}"
     local plan_file="${2}"
 
-    if [ -z "${module}" ]; then
-        echo 'Unspecified module.'
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
         return 1
     fi
 
     if [ -z "${plan_file}" ]; then
-        plan_file='../plan_file'
+        plan_file='plan_file'
     fi
 
-    "${TERRAFORM}"         \
-        -chdir="${module}" \
-        apply              \
-        -auto-approve      \
-        -input=false       \
+    "${TERRAFORM}"            \
+        -chdir="${directory}" \
+        apply                 \
+        -auto-approve         \
+        -input=false          \
         "${plan_file}"
 }
 
 
-# Call 'terraform destroy' for a module
+# Call 'terraform destroy' for a directory
 tf_destroy() {
-    local module="${1}"
+    local directory="${1}"
     local plan_file="${2}"
 
-    tf_apply "${module}" "${plan_file}"
+    tf_apply "${directory}" "${plan_file}"
+}
+
+
+# Call 'terraform output' for a directory
+tf_output() {
+    local directory="${1}"
+
+    if [ -z "${directory}" ]; then
+        echo 'Unspecified directory.'
+        return 1
+    fi
+
+    "${TERRAFORM}"            \
+        -chdir="${directory}" \
+        output
 }
 
 
@@ -238,6 +253,7 @@ if [ -z "${TFLINT}" ]; then
 fi
 
 action="${1}"
-module="${2}"
-varfile="${3}"
-"tf_${action}" "${module}" "${varfile}"
+directory="${2}"
+first_arg="${3}"
+second_arg="${4}"
+"tf_${action}" "${directory}" "${first_arg}" "${second_arg}"
