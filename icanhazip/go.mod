4bc89b374d8a26093d0629739eeb55da74454801
Tyler Tidman <tyler.tidman@draak.ca>
2022-10-24 14:21:31 -0400

    Make the icanhazip tool more configurable/controllable

---
+++ icanhazip/go.mod
@@ -0,0 +1,10 @@
+module icanhazip
+
+go 1.19
+
+require (
+	github.com/ian-kent/envconf v0.0.0-20141026121121-c19809918c02
+	github.com/vharitonsky/iniflags v0.0.0-20180513140207-a33cd0b5f3de
+)
+
+require github.com/smartystreets/goconvey v1.7.2 // indirect
---
+++ icanhazip/icanhazip.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"runtime/debug"
+	"strings"
+
+	. "github.com/ian-kent/envconf"
+	"github.com/vharitonsky/iniflags"
+)
+
+// Command-line arguments
+var (
+	aPort     string
+	aProtocol string
+	aVersion  bool
+)
+
+func init() {
+	// Help for command-line arguments
+	const (
+		sPort     = "TCP port number on which to listen"
+		sProtocol = "IP Protocol on which to listen IPv6/v4"
+		sVersion  = "Display build version information (default false)"
+	)
+
+	flag.StringVar(&aPort, "port", FromEnvP("ICANHAZIP_PORT", "8080").(string), sPort)
+	flag.StringVar(&aPort, "p", FromEnvP("ICANHAZIP_PORT", "8080").(string), sPort)
+	flag.StringVar(&aProtocol, "protocol", FromEnvP("ICANHAZIP_PROTOCOL", "4").(string), sProtocol)
+	flag.StringVar(&aProtocol, "t", FromEnvP("ICANHAZIP_PROTOCOL", "4").(string), sProtocol)
+	flag.BoolVar(&aVersion, "version", FromEnvP("ICANHAZIP_VERSION", false).(bool), sVersion)
+	flag.BoolVar(&aVersion, "v", FromEnvP("ICANHAZIP_VERSION", false).(bool), sVersion)
+	iniflags.Parse()
+
+	if flag.NArg() > 0 {
+		fmt.Fprintf(os.Stderr, "Error: Unused command line arguments detected.\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+// go build -ldflags "-X main.Version=$(git describe --always --dirty --tags)"
+var Version string
+
+func GetVersion() string {
+	var barch, bos, bmod, brev, btime, suffix string
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			switch setting.Key {
+			case "GOARCH":
+				barch = setting.Value
+			case "GOOS":
+				bos = setting.Value
+			case "vcs.modified":
+				bmod = setting.Value
+			case "vcs.revision":
+				brev = setting.Value[0:7]
+			case "vcs.time":
+				btime = setting.Value
+			}
+		}
+	}
+	// If we didn't specify a version string, use the git commit
+	if Version == "" {
+		Version = brev
+	}
+	// If the git repo wasn't clean, say so in the version string
+	if bmod == "true" {
+		suffix = "-dirty"
+	}
+	return fmt.Sprintf("%s%s %s %s %s", Version, suffix, bos, barch, btime)
+}
+
+func main() {
+	// Print out the version information
+	if aVersion {
+		fmt.Println(GetVersion())
+		os.Exit(0)
+	}
+
+	sm := http.NewServeMux()
+	sm.HandleFunc("/", showIp)
+
+	fmt.Println(fmt.Sprintf(":%s", aPort))
+
+	var (
+		l   net.Listener
+		err error
+	)
+	switch aProtocol {
+	case "4":
+		l, err = net.Listen("tcp4", fmt.Sprintf(":%s", aPort))
+	case "6":
+		l, err = net.Listen("tcp6", fmt.Sprintf(":%s", aPort))
+	default:
+		fmt.Println("Unsupported protocol")
+		os.Exit(2)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(http.Serve(l, sm))
+}
+
+func showIp(w http.ResponseWriter, r *http.Request) {
+	requesterIpAndPort := r.RemoteAddr
+	requesterIp := r.RemoteAddr[:strings.LastIndex(r.RemoteAddr, ":")]
+
+	fmt.Println(requesterIpAndPort)
+	w.WriteHeader(200)
+	w.Write([]byte(requesterIp + "\n"))
+}
+
+// https://github.com/qwinsi/tiny-server
+// https://go.dev/play/p/EOZkK1UUpe
+// https://gist.github.com/2minchul/191716b3ca8799f53362746731d08e91
+// https://gist.github.com/bacher09/51ce161105a9e1f49b8b917f8eccd3c5
