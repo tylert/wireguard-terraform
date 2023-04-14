183c5f5dc2e31dd0fbadb4cd26ff431742d8625c
Tyler Tidman <tyler.tidman@draak.ca>
2023-04-14 14:14:16 -0400

    Let the example deployment handle more than a single client

--- infra/deployments/dev/main.tf
+++ infra/deployments/dev/main.tf
@@ -25,7 +25,7 @@ module "vpc_core1" {
 module "vpc_rules1" {
   source              = "../../modules/aws/vpc_rules"
   basename            = "tyler1"
-  external_ipv4_addrs = ["100.100.100.100/32"]
+  external_ipv4_addrs = ["100.100.100.100/32", "200.200.200.200/32"]
   external_ipv6_addrs = []
   depends_on          = [module.vpc_core1]
 }
@@ -54,7 +54,7 @@ module "vpc_core2" {
 module "vpc_rules2" {
   source              = "../../modules/aws/vpc_rules"
   basename            = "tyler2"
-  external_ipv4_addrs = ["100.100.100.100/32"]
+  external_ipv4_addrs = ["100.100.100.100/32", "200.200.200.200/32"]
   external_ipv6_addrs = []
   depends_on          = [module.vpc_core2]
 
