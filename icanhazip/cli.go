fc1fe3047737360943d71752634f1cacec46dc2c
Tyler Tidman <tyler.tidman@draak.ca>
2023-09-29 16:04:54 -0400

    Rename help to usage

--- icanhazip/cli.go
+++ icanhazip/cli.go
@@ -17,19 +17,19 @@ var (
 )
 
 func init() {
-	// Help for command-line arguments
+	// Usage for command-line arguments
 	const (
-		sPort    = "TCP port number on which to listen 0-65535"
-		sProto   = "IP version on which to listen IPv6/v4"
-		sVersion = "Display build version information (default false)"
+		uPort    = "TCP port number on which to listen 0-65535"
+		uProto   = "IP version on which to listen IPv6/v4"
+		uVersion = "Display build version information (default false)"
 	)
 
-	flag.StringVar(&aProto, "ipv", FromEnvP("ICANHAZIP_PROTO", "4").(string), sProto)
-	flag.StringVar(&aProto, "i", FromEnvP("ICANHAZIP_PROTO", "4").(string), sProto)
-	flag.StringVar(&aPort, "port", FromEnvP("ICANHAZIP_PORT", "8080").(string), sPort)
-	flag.StringVar(&aPort, "p", FromEnvP("ICANHAZIP_PORT", "8080").(string), sPort)
-	flag.BoolVar(&aVersion, "version", FromEnvP("ICANHAZIP_VERSION", false).(bool), sVersion)
-	flag.BoolVar(&aVersion, "v", FromEnvP("ICANHAZIP_VERSION", false).(bool), sVersion)
+	flag.StringVar(&aProto, "ipv", FromEnvP("ICANHAZIP_PROTO", "4").(string), uProto)
+	flag.StringVar(&aProto, "i", FromEnvP("ICANHAZIP_PROTO", "4").(string), uProto)
+	flag.StringVar(&aPort, "port", FromEnvP("ICANHAZIP_PORT", "8080").(string), uPort)
+	flag.StringVar(&aPort, "p", FromEnvP("ICANHAZIP_PORT", "8080").(string), uPort)
+	flag.BoolVar(&aVersion, "version", FromEnvP("ICANHAZIP_VERSION", false).(bool), uVersion)
+	flag.BoolVar(&aVersion, "v", FromEnvP("ICANHAZIP_VERSION", false).(bool), uVersion)
 	iniflags.Parse()
 
 	if flag.NArg() > 0 {
