114c36bbcbc8271fdd167f24188315a659c8d420
Tyler Tidman <tyler.tidman@draak.ca>
2023-11-24 14:47:16 -0500

    Remove redundant comments

--- infra/modules/resource/dynamic_dns/0_module_interface.tf
+++ infra/modules/resource/dynamic_dns/0_module_interface.tf
@@ -34,7 +34,7 @@ terraform {
 
 data "aws_caller_identity" "current" {} # account_id
 
-data "aws_region" "current" {} # region
+data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {
   state = "available"
