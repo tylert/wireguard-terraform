71af76dd57d38b132b19a9b4d9f72f399e0d8eb2
Tyler Tidman <tyler.tidman@draak.ca>
2024-01-22 11:58:38 -0500

    Minor tweak to apply script

--- apply.sh
+++ apply.sh
@@ -9,14 +9,12 @@ fi
 terraform_bin='terraform'
 plan_file='plan_file.zip'
 
-# Do an init, followed by a plan, pause, then do an apply
-# Hit ctrl-c to abort the deployment (before ending the pause to do it cleanly)
+# Do an init, followed by a validate, plan, pause, then do an apply
+# Hit ctrl-c once to abort things (before ending the pause to do it cleanly)
 "${terraform_bin}" -chdir="${layer_dir}" init -input=false -upgrade=true
+"${terraform_bin}" -chdir="${layer_dir}" validate
 "${terraform_bin}" -chdir="${layer_dir}" plan ${EXTERMINATE_EXTERMINATE} -input=false -out="${plan_file}"
 
 echo 'Pause for reflection before applying everything...'
 read
 "${terraform_bin}" -chdir="${layer_dir}" apply -auto-approve -input=false "${plan_file}"
-
-# Clean up the stale plan file
-rm -f "${layer_dir}/${plan_file}"
