c9984c380db1c1175b7a0561eed71f957f2508ee
Tyler Tidman <tyler.tidman@draak.ca>
2024-01-29 21:10:54 -0500

    Remove unneeded lib for env var thingy

--- icanhazip/go.mod
+++ icanhazip/go.mod
@@ -1,7 +1,3 @@
 module icanhazip
 
 go 1.21.5
-
-require github.com/ian-kent/envconf v0.0.0-20141026121121-c19809918c02
-
-require github.com/smartystreets/goconvey v1.7.2 // indirect
--- icanhazip/params.go
+++ icanhazip/params.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
-
-	. "github.com/ian-kent/envconf"
+	"strconv"
+	"strings"
 	//"gopkg.in/ini.v1"
 )
 
@@ -40,7 +41,7 @@ func init() {
 	}
 
 	// FlagSet for sub-commands???
-	// https://www.digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
+	// https://digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
 
 	// Attempt to gracefully load things from a known config file location
 	// cfg := ini.Empty()
@@ -53,5 +54,85 @@ func init() {
 		fmt.Fprintf(os.Stderr, "Error: Unused command line arguments detected.\n")
 		flag.Usage()
 		os.Exit(1)
+	}
+}
+
+var (
+	// ErrUnsupportedType is returned if the type passed in is unsupported
+	ErrUnsupportedType = errors.New("Unsupported type")
+)
+
+// FromEnvP is the same as FromEnv, but panics on error
+func FromEnvP(env string, value interface{}) interface{} {
+	ev, err := FromEnv(env, value)
+	if err != nil {
+		panic(err)
+	}
+	return ev
+}
+
+// FromEnv returns the environment variable specified by env
+// using the type of value
+func FromEnv(env string, value interface{}) (interface{}, error) {
+	envs := os.Environ()
+	found := false
+	for _, e := range envs {
+		if strings.HasPrefix(e, env+"=") {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return value, nil
+	}
+
+	ev := os.Getenv(env)
+
+	switch value.(type) {
+	case string:
+		vt := interface{}(ev)
+		return vt, nil
+	case int:
+		i, e := strconv.ParseInt(ev, 10, 64)
+		return int(i), e
+	case int8:
+		i, e := strconv.ParseInt(ev, 10, 8)
+		return int8(i), e
+	case int16:
+		i, e := strconv.ParseInt(ev, 10, 16)
+		return int16(i), e
+	case int32:
+		i, e := strconv.ParseInt(ev, 10, 32)
+		return int32(i), e
+	case int64:
+		i, e := strconv.ParseInt(ev, 10, 64)
+		return i, e
+	case uint:
+		i, e := strconv.ParseUint(ev, 10, 64)
+		return uint(i), e
+	case uint8:
+		i, e := strconv.ParseUint(ev, 10, 8)
+		return uint8(i), e
+	case uint16:
+		i, e := strconv.ParseUint(ev, 10, 16)
+		return uint16(i), e
+	case uint32:
+		i, e := strconv.ParseUint(ev, 10, 32)
+		return uint32(i), e
+	case uint64:
+		i, e := strconv.ParseUint(ev, 10, 64)
+		return i, e
+	case float32:
+		i, e := strconv.ParseFloat(ev, 32)
+		return float32(i), e
+	case float64:
+		i, e := strconv.ParseFloat(ev, 64)
+		return float64(i), e
+	case bool:
+		i, e := strconv.ParseBool(ev)
+		return i, e
+	default:
+		return value, ErrUnsupportedType
 	}
 }
